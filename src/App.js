3aa8a402a744c5764f1ebdcd53b5604e9be4487c
Sandino <sandinoandroid@gmail.com>
2018-11-07 23:18:00 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Login from './pages/Login'
+import Timeline from './pages/Timeline'
+
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact={true} Component={Login}/>
+          <Route path="/timeline" Component={Timeline} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
---
+++ src/pages/Timeline.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <h1>
+        Timeline
+      </h1>
+    )
+  }
+}
